4292e5a138102166b99d8cddc59c2718cb51c69b
Brigette Tomas <74156200+bm35b@users.noreply.github.com>
2023-05-03 15:55:15 -1000

    Add python

---
+++ main.py
@@ -0,0 +1,14 @@
+# Survey123 data exploration 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+url='https://drive.google.com/file/d/1bayMcfy2Iw_SqyeWTmyscAMzPGx2JBaf/view?usp=sharing'
+url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+data = pd.read_csv(url)
+
+column_name = "Are there no crops (fallow or pasture), one crop, or multiple crops growing in the field?"
+ax = data[column_name].value_counts().plot(kind='bar')
+ax.set_xlabel("Field types")
+ax.set_ylabel("Number of points")
+plt.show()
+
